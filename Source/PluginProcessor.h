8891c391fdacc775de266b8602f5e198a7448835
Emir Gocturk <emir.gocturk@axign.nl>
2021-05-14 23:26:44 +0200

    Osc declaration
    private function

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -54,6 +54,7 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    juce::dsp::Oscillator<float> osc{ [](float x) { return std::sin(x); } };
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BasicSynthAudioProcessor)
 };
