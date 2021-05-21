e84eea846f3ed74887a43a119ab52a721358de25
Emir Gocturk <emir.gocturk@axign.nl>
2021-05-21 19:56:09 +0200

    synth sound init

---
+++ Source/SynthSound.h
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    SynthSound.h
+    Created: 21 May 2021 7:52:33pm
+    Author:  emir.gocturk
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class SynthSound : public juce::SynthesiserSound
+{
+public:
+    bool appliesToNote(int midiNoteNumber) override { return true; }
+    bool appliesToChannel(int midiChannel) orverride { return true; }
+};
