cc4feaf545f38d4f3bd20045b262e707f2fe8bbd
Emir Gocturk <emir.gocturk@axign.nl>
2021-05-21 19:59:21 +0200

    Prototypes voice

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -11,3 +11,14 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "SynthSound.h"
+
+class SynthVoice : public juce::SynthesiserVoice
+{
+public:
+    bool canPlaySound(juce::SynthesiserSound*) override { return true; }
+    void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    void stopNote(float velocity, bool allowTailOff) override;
+private:
+
+};
